85b7e2b632537666cdacab398443136d5cddb3a4
maulana2021 <80517223+maulana2021@users.noreply.github.com>
2021-03-19 00:19:22 +0800

    Upload tamplate
    upload aja
    upload

---
+++ lightmodescript.js
@@ -0,0 +1,18 @@
+
+
+const options = {
+    bottom: '20px',
+    right: 'unset',
+    left: '20px',
+    time: '0.5s',
+    mixColor: '#fff',
+    backgroundColor: '#fff',
+    buttonColorDark: '#100f2c',
+    buttonColorLight: '#fff',
+    saveInCookies: false,
+    label: '🌓', //
+    autoMatchOsTheme: true
+}
+
+const darkmode = new Darkmode(options);
+darkmode.showWidget();
